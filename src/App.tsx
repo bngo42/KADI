38ee17a518fe4eb8624b2294a241aed637d5d896
Bngo <bobi.ngo@staffmatch.com>
2022-08-14 15:49:13 +0200

    Update project with typescript & scss

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.scss';
+import Logo from './assets/logo.png';
+
+function App() {
+  return (
+    <div className="wrapper">
+        <header className="header">
+            <img height="100%" src={ Logo } alt="Logo"/>
+            <button className="btn">Ajouter une liste</button>
+        </header>
+
+        <div className="main-content">
+
+        </div>
+    </div>
+  );
+}
+
+export default App;
